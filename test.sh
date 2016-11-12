189e0bc214113ca1083469c9e2e1fc4426ddb8b3
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2016-11-12 16:23:55 -0600

    Cleanup

--- test.sh
+++ test.sh
@@ -2,19 +2,27 @@
 
 BASE_LOCAL_PORT=5000
 NUM_LOCAL_PORTS=10
+
 BASE_REMOTE_PORT=10000
 NUM_REMOTE_PORTS=1
+
+CONNECT_TIMEOUT=2000
+
 CMD="./cproxy"
+
 i=0
 while [ ${i} -lt ${NUM_LOCAL_PORTS} ]; do
   CMD="${CMD} -l 0.0.0.0:$((${BASE_LOCAL_PORT} + ${i}))"
   i=$((${i} + 1))
 done
+
 i=0
 while [ ${i} -lt ${NUM_REMOTE_PORTS} ]; do
   CMD="${CMD} -r 127.0.0.1:$((${BASE_REMOTE_PORT} + ${i}))"
   i=$((${i} + 1))
 done
-CMD="${CMD} -c 2000"
+
+CMD="${CMD} -c ${CONNECT_TIMEOUT}"
+
 echo "${CMD}"
 $CMD
